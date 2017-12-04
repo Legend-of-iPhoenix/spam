255e84fef38340c0fa4fb495ed682e2a6de302c2
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-12-04 16:40:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var n = [];
+var j = [];
 var lastMessage = "";
 var numMessages = -1;
 
@@ -8,100 +9,115 @@ function getCookie(name) {
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
-window.onload = function() {
-  if (document.cookie.indexOf("spam_uid") == -1)
-  {
-    firebase.database().ref("uid").transaction(function(value) {
-    	value++;
+window.onload = function () {
+	var canvas = document.getElementById("stats");
+	canvas.width = window.innerWidth;
+	canvas.height = 2*window.innerHeight/3;
+  if (document.cookie.indexOf("spam_uid") == -1) {
+    firebase.database().ref("uid").transaction(function (value) {
+      value++;
       document.cookie = "spam_uid=" + value;
       uid = value;
       return value;
     });
-    for (var i = 0; i < 64; i++)
-		{
-			n[i] = 0;
-		}
+    for (var i = 0; i < 65; i++) {
+      n[i] = 0;
+    }
+  } else {
+    uid = getCookie("spam_uid");
+    firebase.database().ref("users/" + getCookie("spam_uid") + "/data").once('value').then(function (snapshot) {
+      n = snapshot.val();
+      var canvas = document.getElementById("stats");
+    	var ctx = canvas.getContext("2d");
+    	ctx.fillStyle = "#000";
+    	ctx.font = '20px "Lucida Console", Monaco, monospace';
+    	var increment = ((canvas.height-20) / Math.max(...n));
+    	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    	for (var i = 0; i < n.length; i++) {
+     	 ctx.fillRect(i * canvas.width / n.length, (canvas.height - 25) - increment * n[i], canvas.width / n.length, increment * n[i]);
+   	   ctx.fillText(string.substr(i,1), i * canvas.width / n.length, canvas.height);
+  	  }
+    });
   }
-  else
-  {
-  	uid = getCookie("spam_uid");
-  	firebase.database().ref("users/"+getCookie("spam_uid")+"/data").once('value').then(function(snapshot) {
-  		n = snapshot.val();
-  	});
-  }
-  document.getElementById("a").addEventListener("keydown", function(event) {
-  //event.preventDefault();
-  if (event.keyCode === 13) {
-  	submitMessage();
-  }
-  if (document.getElementById("a").value.length >= 100 && event.keyCode != 8) {
-  	event.preventDefault();
-  }
-  else
-  {
-  	return false;
-  }
-});
+  document.getElementById("a").addEventListener("keydown", function (event) {
+    //event.preventDefault();
+    if (event.keyCode === 13) {
+      submitMessage();
+    }
+
+    if (document.getElementById("a").value.length < 100)
+    {
+    	j = []
+    	var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 *";
+    	var count = 0;
+    	var a = document.getElementById("a").value;
+    	for (var i = 0; i < 63; i++) {
+        var re = new RegExp("[^" + string.substr(i, 1) + "]", "g");
+        j[i] = n[i]+a.replace(re, "").length;
+        count += a.replace(re, "").length;
+      }
+      j[63] = n[63] + a.length - count;
+		var canvas = document.getElementById("stats");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#000";
+    ctx.font = '20px "Lucida Console", Monaco, monospace';
+    var increment = ((canvas.height-20) / Math.max(...j));
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < j.length; i++) {
+      ctx.fillRect(i * canvas.width / j.length, (canvas.height - 25) - increment * j[i], canvas.width / j.length, increment * j[i]);
+      ctx.fillText(string.substr(i,1), i * canvas.width / j.length, canvas.height);
+    }
+    }
+    if (document.getElementById("a").value.length >= 100 && event.keyCode != 8) {
+      event.preventDefault();
+    } else {
+      return false;
+    }
+  });
+
 }
 
 function submitMessage() {
   var a = document.getElementById('a').value;
-  if (a != lastMessage)
-  {
-  	lastMessage = a;
-  if (a.length > 10 && a.length <= 100) {
-    firebase.database().ref('spam').push(a);
-    document.getElementById('a').value='';
-    var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
-    var stats = document.getElementById("stats");
-    stats.innerHTML = "<tr><th>Character</th><th>Quantity</th></tr>";
-    var count = 0;
-    for (var i = 0; i < 63; i++)
-    {
-    	var node = document.createElement("tr");
-    	var re = new RegExp("[^"+string.substr(i,1)+"]","g");
-    	console.log(re);
-    	console.log(a.replace(re, ""));
-    	n[i] += a.replace(re, "").length;
-    	count += a.replace(re, "").length;
-    	var subNode1 = document.createElement("td");
-    	subNode1.innerText = "'"+string.substr(i,1)+"'";
-    	node.appendChild(subNode1);
-    	var subNode2 = document.createElement("td");
-    	subNode2.innerText = n[i];
-    	node.appendChild(subNode2);
-    	stats.appendChild(node);
+  if (a != lastMessage) {
+    lastMessage = a;
+    if (a.length > 10 && a.length <= 100) {
+      firebase.database().ref('spam').push(a);
+      document.getElementById('a').value = '';
+      var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 *";
+      var stats = document.getElementById("stats");
+      stats.innerHTML = "<tr><th>Character</th><th>Quantity</th></tr>";
+      var count = 0;
+      for (var i = 0; i < 63; i++) {
+        var re = new RegExp("[^" + string.substr(i, 1) + "]", "g");
+        console.log(re);
+        console.log(a.replace(re, ""));
+        n[i] += a.replace(re, "").length;
+        count += a.replace(re, "").length;
+      }
+      n[63] += a.length - count;
+      numMessages++;
+      if (numMessages % 10 == 0) {
+        firebase.database().ref("users/" + getCookie("spam_uid") + "/data").set(n);
+      }
+    var canvas = document.getElementById("stats");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#000";
+    ctx.font = '20px "Lucida Console", Monaco, monospace';
+    var increment = ((canvas.height-20) / Math.max(...n));
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < n.length; i++) {
+      ctx.fillRect(i * canvas.width / n.length, (canvas.height - 25) - increment * n[i], canvas.width / n.length, increment * n[i]);
+      ctx.fillText(string.substr(i,1), i * canvas.width / n.length, canvas.height);
     }
-    n[64] = a.length - count;
-    	var node = document.createElement("tr");
-      var subNode1 = document.createElement("td");
-    	subNode1.innerText = "Other";
-    	node.appendChild(subNode1);
-    	var subNode2 = document.createElement("td");
-    	subNode2.innerText = n[64];
-    	node.appendChild(subNode2);
-    	stats.appendChild(node);
-    	node = document.createElement("tr");
-      subNode1 = document.createElement("td");
-    	subNode1.innerHTML = "<strong>Total</strong>";
-    	node.appendChild(subNode1);
-    	subNode2 = document.createElement("td");
-    	subNode2.innerText = n.reduce(add, 0);
-    	node.appendChild(subNode2);
-    	stats.appendChild(node);
-    	numMessages++;
-    	if (numMessages % 10 == 0)
-    	{
-    		firebase.database().ref("users/"+getCookie("spam_uid")+"/data").set(n);
-    	}
   }
 }
 }
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
-window.onbeforeunload = function() {
-	firebase.database().ref("users/"+getCookie("spam_uid")+"/data").set(n);
+window.onbeforeunload = function () {
+  firebase.database().ref("users/" + getCookie("spam_uid") + "/data").set(n);
 }
