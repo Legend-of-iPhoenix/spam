7149bc63413205115ed3e43ea78739ef573cb7df
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-12-04 17:41:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ function submitMessage() {
       if (numMessages % 10 == 0) {
         firebase.database().ref("users/" + getCookie("spam_uid") + "/data").set(n);
       }
-	    document.getElementById('total').value = "Total characters: "+ n.reduce(add, 0);
+	    document.getElementById('total').innerText = "Total characters: "+ n.reduce(add, 0);
     var canvas = document.getElementById("stats");
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "#000";
